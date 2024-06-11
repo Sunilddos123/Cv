a0d81576911fbae191a138093ceb3e47b256ea9e
Sunilddos123 <rockeyvaishnav405@gmail.com>
2024-06-11 20:55:54 +0530

    Update m.py

--- m.py
+++ m.py
@@ -11,7 +11,7 @@ import os
 bot = telebot.TeleBot('7454964661:AAHdwBhy6QCpH5vT2wpfotZirDqXB-mw4eA')
 
 # Admin user IDs
-admin_id = ["6769245930,1002220511003"]
+admin_id = ["6769245930"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
